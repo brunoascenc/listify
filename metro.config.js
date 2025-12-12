8ca6617772fee1067ef8c6ae30bfb3dc6d9de58b
bruno <bruno@i9tech.co>
2025-12-12 09:30:37 -0300

    chore: tailwind and react navigation config

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,14 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { withUniwindConfig } = require('uniwind/metro'); 
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+const config = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+// your metro modifications
+
+module.exports = withUniwindConfig(config, {  
+  // relative path to your global.css file (from previous step)
+  cssEntryFile: './src/global.css',
+  // (optional) path where we gonna auto-generate typings
+  // defaults to project's root
+  dtsFile: './src/uniwind-types.d.ts'
+});
